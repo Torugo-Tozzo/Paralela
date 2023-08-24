5fecc063957cd8803a3fa290b3c067209e6faa07
Victor H. T. Filho <114814350+Torugo-Tozzo@users.noreply.github.com>
2023-08-24 16:42:34 -0300

    Update ping_pong.c

--- ping_pong.c
+++ ping_pong.c
@@ -1,117 +1,48 @@
-/*                                                                      */
-/* CS 4331: Parallel Programming                        Fall, 2009      */
-/*                                                                      */
-/* This MPI program computes does a "ping-pong" message passing		*/
-/* performance measurement.						*/
-/*                                                                      */
+// Author: Wes Kendall
+// Copyright 2011 www.mpitutorial.com
+// This code is provided freely with the tutorials on mpitutorial.com. Feel
+// free to modify it for your own use. Any distribution of the code must
+// either provide a link to www.mpitutorial.com or keep this header intact.
+//
+// Ping pong example with MPI_Send and MPI_Recv. Two processes ping pong a
+// number back and forth, incrementing it until it reaches a given value.
+//
+#include <mpi.h>
 #include <stdio.h>
-#include "mpi.h"
+#include <stdlib.h>
 
-#define  MAX_LEN 1 << 18	/* maximum vector length	*/
-#define  TRIALS  100		/* trials for each msg length	*/
-#define  PROC_0  0		/* processor 0			*/
-#define  B0_TYPE 176		/* message "types"		*/
-#define  B1_TYPE 177
+int main(int argc, char** argv) {
+  const int PING_PONG_LIMIT = 10;
 
-int main(argc,argv)
-    int argc;
-    char *argv[];
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL);
+  // Find out rank, size
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-{
-    int numprocs, p,		/* number of processors, proc index	*/
-	myid,			/* this processor's "rank"		*/
-	length,			/* vector length			*/
-	i, t;			
+  // We are assuming 2 processes for this task
+  if (world_size != 2) {
+    fprintf(stderr, "World size must be two for %s\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
 
-    double b0[MAX_LEN], b1[MAX_LEN];	/* vectors			*/
-
-    double start_time, end_time;	/* "wallclock" times		*/
-
-    MPI_Status stat;		/* MPI structure containing return	*/
-				/* codes for message passing operations */
-
-    MPI_Request send_handle, recv_handle;	/* For nonblocking msgs	*/
-
-    MPI_Init(&argc,&argv);			/* initialize MPI	*/
-
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);	/*how many processors?	*/
-    MPI_Comm_rank(MPI_COMM_WORLD, &myid);	/*which one am I?	*/
-
-    if (myid == PROC_0)
-	{
-	srand48(0xFEEDFACE);
-
-	/* generate processor 0's vector */
-
-	for (i=0; i<MAX_LEN; ++i)
-	    b0[i] = (double) drand48();
-	}
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    /* warmup, if necessary */
-/*
-    printf("# Start warmup.\n");
-
-    for (length=1; length<=MAX_LEN; length*=2)
-	if (myid == PROC_0)
-	{
-		MPI_Send(b0, length, MPI_DOUBLE, 1, B1_TYPE, MPI_COMM_WORLD);
-		MPI_Recv(b1, length, MPI_DOUBLE, 1, B0_TYPE, MPI_COMM_WORLD, &stat);
-	}
-	else
-	{
-		MPI_Recv(b1, length, MPI_DOUBLE, 0, B1_TYPE, MPI_COMM_WORLD, &stat);
-		MPI_Send(b0, length, MPI_DOUBLE, 0, B0_TYPE, MPI_COMM_WORLD);
-	}
-*/
-    if (myid == PROC_0)
-#ifdef BLOCKING
-	printf("\nPing-pong measurements for blocking send/recv pairs.\n");
-#else
-	printf("\nPing-pong measurements for nonblocking send/recv pairs.\n");
-#endif
-    /* measure message passing speed for vectors of various lengths */
-
-    for (length=1; length<=MAX_LEN; length*=2)
-    {
-	MPI_Barrier(MPI_COMM_WORLD);
-
-	if (myid == PROC_0)
-		start_time = MPI_Wtime();
-
-	for (t=0; t<TRIALS; ++t)
-	{
-#ifdef BLOCKING
-		if (myid == PROC_0)
-		{
-			MPI_Send(b0, length, MPI_DOUBLE, 1, B1_TYPE, MPI_COMM_WORLD);
-			MPI_Recv(b1, length, MPI_DOUBLE, 1, B0_TYPE, MPI_COMM_WORLD, &stat);
-		}
-		else
-		{
-			MPI_Recv(b1, length, MPI_DOUBLE, 0, B1_TYPE, MPI_COMM_WORLD, &stat);
-			MPI_Send(b0, length, MPI_DOUBLE, 0, B0_TYPE, MPI_COMM_WORLD);
-		}
-#else
-		MPI_Isend(b0, length, MPI_DOUBLE, (myid+1)%numprocs, B0_TYPE, MPI_COMM_WORLD, &send_handle);
-		MPI_Irecv(b1, length, MPI_DOUBLE, (myid+1)%numprocs, B0_TYPE, MPI_COMM_WORLD, &recv_handle);
-
-		MPI_Wait(&send_handle, &stat);
-		MPI_Wait(&recv_handle, &stat);
-#endif
-	}
-	if (myid == PROC_0)
-	    {
-	    end_time = MPI_Wtime();
-
-	    /* gnuplot ignores output lines that begin with #. */
-	    printf("# Length = %d\tAverage time=%lf\n",
-		length, (end_time - start_time)/(double)(2*TRIALS));
-	    printf(" %d\t %lf\n",
-		length, (end_time - start_time)/(double)(2*TRIALS));
-	    }
+  int ping_pong_count = 0;
+  int partner_rank = (world_rank + 1) % 2;
+  while (ping_pong_count < PING_PONG_LIMIT) {
+    if (world_rank == ping_pong_count % 2) {
+      // Increment the ping pong count before you send it
+      ping_pong_count++;
+      MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
+      printf("%d sent and incremented ping_pong_count %d to %d\n",
+             world_rank, ping_pong_count, partner_rank);
+    } else {
+      MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      printf("%d received ping_pong_count %d from %d\n",
+             world_rank, ping_pong_count, partner_rank);
     }
-
-    MPI_Finalize();
+  }
+  MPI_Finalize();
 }
